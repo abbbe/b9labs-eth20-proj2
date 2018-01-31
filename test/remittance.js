6a663ef9c171ceb027c4d731facd851f75111f6f
Alexandre Bezroutchko <abb@gremwell.com>
2018-01-31 07:26:53 +0100

    added deployment routines and deployment tests

--- test/remittance.js
+++ test/remittance.js
@@ -1,7 +1,21 @@
-contract('remittance', function(accounts) {
-  it("should assert true", function(done) {
-    var remittance = remittance.deployed();
-    assert.isTrue(true);
-    done();
+Promise = require("bluebird");
+Promise.promisifyAll(web3.eth, { suffix: "Promise" });
+
+// web3.eth.getTransactionReceiptMined = require("./helpers/getTransactionReceiptMined.js");
+// const expectedExceptionPromise = require("./helpers/expectedExceptionPromise.js");
+
+var Remittance = artifacts.require("./Remittance.sol");
+
+contract('Remittance', function(accounts) {
+  var remittance;
+  
+  beforeEach("deploy", function() {
+    return Remittance.deployed().then(instance => {
+      remittance = instance;
+    })
+  });
+
+  it("should deploy", function() {
+    assert.notEqual(remittance.contract.address, 0);
   });
 });
--- truffle.js
+++ truffle.js
@@ -1,4 +1,51 @@
+var web3 = require('web3');
+var net = require('net');
+var ipcPath = '/Users/abb/data/geth-net24601-ipc/geth.ipc';
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+
+    net24601: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "24601"
+    },
+
+    net1337: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "1337"
+    },
+
+    net24601_ipc: {
+      provider: function () {
+        return new web3.providers.IpcProvider(ipcPath, net);
+      },
+      network_id: "24601"
+    },
+
+    parity_dvp: {
+      host: "127.0.0.1",
+      port: 48545,
+      network_id: "24601"
+    },
+
+    burp: {
+      host: "127.0.0.1",
+      port: 4545,
+      network_id: "*"
+    },
+
+    geth_rinkeby: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "42",
+      from: "0xe2DBC1817A18d345d051a348ceF998f3c14C2033"
+    }
+  }
 };

792ab6f5848088042a05cecee52a245a5af0b19e
Alexandre Bezroutchko <abb@gremwell.com>
2018-02-03 12:09:04 +0100

    fix #10 make OwnableKillable separate file

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -1,34 +1,6 @@
 pragma solidity ^0.4.18;
 
-contract OwnableKillable {
-  address public owner;
-  bool public killed;
-
-  event LogKilled();
-
-  modifier onlyOwner() {
-    require(msg.sender == owner);
-    _;
-  }
-
-  modifier notKilled() {
-    require(!killed);
-    _;
-  }
-
-  function OwnableKillable() internal {
-    owner = msg.sender;
-  }
-
-  function kill()
-    onlyOwner()
-    notKilled()
-    public
-  {
-    LogKilled();
-    killed = true;
-  }
-}
+import "./OwnableKillable.sol";
 
 contract Remittance is OwnableKillable {
   struct RemittanceInfo {
